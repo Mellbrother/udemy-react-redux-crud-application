f09ef557c024b9faa1dcc63e969ea6c50e9f4da0
Keisuke Nemura <nemuraystk@gmail.com>
2020-07-25 00:44:14 +0900

    implement events index

---
+++ src/reducers/events.js
@@ -0,0 +1,14 @@
+import _ from 'lodash'		//responseの形を変形させる
+import { READ_EVENTS } from '../actions'
+
+export default (state = {},  action) => {
+	switch (action.type) {
+		case READ_EVENTS:
+			// console.log(action.response.data)
+			// console.log(_.mapKeys(action.response.data, 'id'))
+
+			return _.mapKeys(action.response.data, 'id')
+		default:
+			return state
+	}
+}
