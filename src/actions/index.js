a4a904bf7a32f4975504348e3b94c79e63193faa
Keisuke Nemura <nemuraystk@gmail.com>
2020-07-25 16:03:04 +0900

    implement event new component

--- src/actions/index.js
+++ src/actions/index.js
@@ -1,6 +1,8 @@
 import axios from 'axios'
 
 export const READ_EVENTS = 'READ_EVENTS'
+export const CREATE_EVENT = 'CREATE_EVENT'
+
 
 const ROOT_URL = 'https://udemy-utils.herokuapp.com/api/v1'
 const QUERYSTRING = '?token=token123'
@@ -8,5 +10,11 @@ const QUERYSTRING = '?token=token123'
 export const readEvents = () => async dispach => {
 	const response = await axios.get(`${ROOT_URL}/events${QUERYSTRING}`)
 	dispach({ type: READ_EVENTS, response })
+
+}
+
+export const postEvent = values => async dispach => {
+	const response = await axios.post(`${ROOT_URL}/events/${QUERYSTRING}`, values)
+	dispach({ type: CREATE_EVENT, response })
 
 }
